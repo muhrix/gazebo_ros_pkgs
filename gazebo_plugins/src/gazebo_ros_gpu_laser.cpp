052c7b7548e16dfa23878e9ec300752d11ece9b1
Murilo FM <muhrix@gmail.com>
2015-07-02 14:26:50 +0100

    Allow empty tf_prefix instead of defaulting to robot namespace

--- gazebo_plugins/src/gazebo_ros_gpu_laser.cpp
+++ gazebo_plugins/src/gazebo_ros_gpu_laser.cpp
@@ -128,11 +128,9 @@ void GazeboRosLaser::LoadThread()
 
   this->rosnode_ = new ros::NodeHandle(this->robot_namespace_);
 
+  // allow tf_prefix to be empty, not forcing it to be the same as robot namespace
   this->tf_prefix_ = tf::getPrefixParam(*this->rosnode_);
-  if(this->tf_prefix_.empty()) {
-      this->tf_prefix_ = this->robot_namespace_;
-      boost::trim_right_if(this->tf_prefix_,boost::is_any_of("/"));
-  }
+
   ROS_INFO("GPU Laser Plugin (ns = %s) <tf_prefix_>, set to \"%s\"",
              this->robot_namespace_.c_str(), this->tf_prefix_.c_str());
 
